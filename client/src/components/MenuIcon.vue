c301672aaef52552a6df8e25e157cd43044731ce
Roger <38413630+icarusgk@users.noreply.github.com>
2022-06-28 20:09:10 -0600

    Add Icons and logo

---
+++ client/src/components/MenuIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="title">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 30px;
+  border-radius: 10px;
+  color: white;
+  transition: background-color 0.1s ease-in-out;
+}
+
+.item:hover {
+  background-color: var(--vivid-red);
+  cursor: pointer;
+}
+</style>
