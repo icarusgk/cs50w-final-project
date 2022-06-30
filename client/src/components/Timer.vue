990e09e72ad937dd4f846f4eeaacd23b7987fed4
Roger <38413630+icarusgk@users.noreply.github.com>
2022-06-30 11:02:37 -0600

    feat: Timer implelementation

--- client/src/components/Timer.vue
+++ client/src/components/Timer.vue
@@ -1,11 +1,66 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue'
+import dayjs from 'dayjs';
+
+const MINUTES25 = dayjs().set('minutes', 25).set('seconds', 0)
+
+let timerId: number
+const timerMinutes = ref(MINUTES25)
+let done = ref(false)
+let ongoing = ref(false)
+
+
+watch(timerMinutes, (newMinutes) => {
+  const minutes = newMinutes.minute()
+  const seconds = newMinutes.second()
+
+  if (minutes === 0 && seconds === 0) {
+    done.value = true
+    stopTimer()
+  }
+})
+
+const toggleOngoing = () => ongoing.value = !ongoing.value
+
+function timer() {
+  // Clear an existing ID
+  if (timerId) { clearInterval(timerId) }
+  // Create new ID
+  timerId = setInterval(() => {
+    timerMinutes.value = timerMinutes.value.subtract(1, 'second')
+  }, 1000)
+}
+
+function startTimer() {
+  // Toggle status
+  toggleOngoing()
+
+  // Check if the timer is done
+  if (done.value) {
+    timerMinutes.value = MINUTES25
+    done.value = false
+  }
+
+  // Start the timer
+  timer()
+}
+
+function stopTimer() {
+  toggleOngoing()
+  clearInterval(timerId)
+}
+</script>
+
 <template>
   <div id="timer-container">
     <!-- Progress -->
     <div id="red-line"></div>
     <!-- Time -->
     <div>
-      <h1 id="timer-count">25:00</h1>
-      <button id="start-timer-btn">Start</button>
+      <h1 id="timer-count">{{ timerMinutes.format("mm:ss") }}</h1>
+      <button @click="startTimer" v-if="!ongoing" id="start-timer-btn">Start</button>
+      <button @click="stopTimer" v-else id="stop-timer-btn">Stop</button>
+      <h2 v-if="done">DONEEE</h2>
     </div>
   </div>
 </template>
@@ -22,13 +77,12 @@
   font-size: 8rem;
 }
 
-#start-timer-btn {
+%timer-button {
   padding: 1rem;
   width: 40%;
   border-radius: 18px;
   font-size: 120%;
   font-family: sans-serif;
-  background-color: var(--vivid-red);
   color: white;
   font-weight: 900;
   border: none;
@@ -37,5 +91,16 @@
   &:hover {
     cursor: pointer;
   }
+}
+
+
+#start-timer-btn {
+  @extend %timer-button;
+  background-color: var(--vivid-red);
+}
+
+#stop-timer-btn {
+  @extend %timer-button;
+  background-color: var(--light-blue);
 }
 </style>
