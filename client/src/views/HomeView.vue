601a025bddf96d330546bc3342e0440dd138c3a5
Roger <38413630+icarusgk@users.noreply.github.com>
2022-06-29 20:38:52 -0600

    Add: Basic timer Style

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import LightingIcon from '../components/icons/LightingIcon.vue'
+</script>
 <template>
   <div id="home">
     <!-- Upper Icons -->
@@ -15,27 +18,26 @@
       </ul>
     </div>
     <!-- Pomo Title -->
-    <div>
-      <div>
-        <!-- Icon -->
-        <!-- Title -->
-        <h1>Pomodoro Timer</h1>
-      </div>
+    <div id="pomodoro-container">
+      <!-- Icon -->
+      <LightingIcon />
+      <!-- Title -->
+      <h1>Pomodoro Timer</h1>
     </div>
     <!-- Third box -->
-    <div>
+    <div id="third-box">
       <!-- Timer -->
-      <div>
+      <div id="timer-container">
         <!-- Progress -->
-        <div></div>
+        <div id="red-line"></div>
         <!-- Time -->
         <div>
-          <h1>25:00</h1>
-          <button>Start</button>
+          <h1 id="timer-count">25:00</h1>
+          <button id="start-timer-btn">Start</button>
         </div>
       </div>
       <!-- Add Tasks -->
-      <div>
+      <div id="tasks-container">
         <!-- Buttons -->
         <div>
           <button>Add new project</button>
@@ -92,14 +94,67 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #home {
   color: white;
   display: flex;
   flex-direction: column;
+  padding: 4rem;
+  width: 100%;
 }
 
-#icons ul li {
-  display: inline-block;
+#icons {
+  ul {
+    text-align: right;
+    li {
+      display: inline-block;
+    }
+  }
 }
+
+#third-box {
+  display: flex;
+  flex-direction: row;
+  margin-top: 4rem;
+}
+
+#pomodoro-container {
+  display: flex;
+  align-items: center;
+  
+}
+
+#red-line {
+  width: 40%;
+  height: 15px;
+  border-radius: 18px;
+  background-color: var(--vivid-red);
+}
+
+#timer-container {
+  width: 50%;
+  margin: 2rem 0 0 1rem;
+}
+
+#tasks-container {
+  width: 50%
+}
+
+#timer-count {
+  font-size: 8rem;
+}
+
+#start-timer-btn {
+  padding: 1rem;
+  width: 20%;
+  border-radius: 18px;
+  font-size: 120%;
+  font-family: sans-serif;
+  background-color: var(--vivid-red);
+  color: white;
+  font-weight: 900;
+  border: none;
+  margin-top: 1rem;
+}
+
 </style>
