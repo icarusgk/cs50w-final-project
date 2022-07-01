2f93fe0861daa50fa0ef147b091f8d9fc030b595
Roger <38413630+icarusgk@users.noreply.github.com>
2022-07-01 12:19:38 -0600

    feat: Add Modal Component
    Provide basic modal child structure

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+// Modal is gonna receive the props
+const props = defineProps<{
+  open: Boolean
+}>()
+
+const emit = defineEmits(['closeModal'])
+
+const closeModal = () => emit('closeModal', false)
+</script>
+
+<template>
+  <Teleport to="body">
+    <div v-if="open" class="modal">
+      <slot></slot>
+      <!-- Close modal -->
+      <button 
+        @click="closeModal()"
+        class="close-modal-button"
+      >Close
+      </button>
+    </div>
+  </Teleport>
+</template>
+
+<style lang="scss" scoped>
+.modal {
+  // Positioning
+  position: fixed;
+  z-index: 2;
+  top: 20%;
+  left: 30%;
+  width: 40%;
+
+  // Color
+  background-color: white;
+  color: black;
+
+  padding: 2rem;
+  border-radius: 10px;
+
+  .close-modal-button {
+    background-color: var(--white);
+    color: var(--black);
+    display: flex;
+    align-items: center;
+    border-radius: 10px;
+    margin-right: 1rem;
+    padding: 0.5rem 0.5rem;
+  }
+}
+</style>
---
+++ client/src/components/NewProjectModal.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Modal from './Modal.vue';
+</script>
+
+<template>
+  <h1>New Project</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
