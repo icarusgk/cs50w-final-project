62145840a726e2aa032a1cf3aec18af10920fcde
Roger <38413630+icarusgk@users.noreply.github.com>
2022-08-11 19:56:19 -0600

    feat: Add Short and Long Breaks

--- client/src/components/Timer.vue
+++ client/src/components/Timer.vue
@@ -16,10 +16,10 @@ watch(() => timer.timer, () => {
   }
 })
 
-function timerInitializer() {
+function initTimer() {
   // Clear an existing ID
   if (timer.timerId) { clearInterval(timer.timerId) }
-  // Create new ID
+  // Create new ID and start a new timer
   timer.timerId = setInterval(() => {
     timer.decrementSecond()
   }, 1000)
@@ -28,33 +28,50 @@ function timerInitializer() {
 function startTimer() {
   // Toggle status
   timer.toggleOngoing()
+  timer.done = false
 
-  // Check if the timer is done
-  if (timer.done) {
-    timer.setInitialValue()
-  }
-
-  // Start the timer
-  timerInitializer()
+  // Restart the timer to the current mode
+  initTimer()
 }
 
 function stopTimer() {
-  timer.toggleOngoing()
+  timer.ongoing = false
   clearInterval(timer.timerId)
-}
+} 
 
 function restartTimer() {
   stopTimer()
-  timer.setInitialValue()
+  switch (timer.current) {
+    case 'pomo':
+      timer.setPomo()
+      break;
+    case 'short':
+      timer.setShortRest()
+      break;
+    case 'long':
+      timer.setLongRest()
+  }
 }
 </script>
 
 <template>
   <div id="timer-container">
+    <div id="timer-setters">
+      <div id="normal-pomo" @click="timer.setPomo(); stopTimer()">
+        Pomo
+      </div>
+      <div id="short-rest" @click="timer.setShortRest(), stopTimer()">
+        Short Rest
+      </div>
+      <div id="long-rest" @click="timer.setLongRest(), stopTimer()">
+        Long Rest
+      </div>
+    </div>
     <!-- Progress -->
     <div 
-      id="red-line"
-      :style="{ 'width': timer.percent + '%' }"
+      id="line"
+      :style="{ 'width': `${timer.percent - 2}%` }"
+      :class="timer.current"
     ></div>
     <!-- Time -->
     <div>
@@ -62,77 +79,138 @@ function restartTimer() {
       <button @click="startTimer()" v-if="!timer.ongoing" id="start-timer-btn">Start!</button>
       <button @click="stopTimer()" v-else id="stop-timer-btn">Stop!</button>
       <button @click="restartTimer()" v-if="timer.ongoing" id="restart-timer-btn">Restart!</button>
-      <h2 v-if="timer.done">DONEEE</h2>
+      <!-- TODO: Change style -->
+      <div class="done" v-if="timer.done">
+        <span>Done!</span>
+      </div>
     </div>
+    
   </div>
 </template>
 
 <style lang="scss" scoped>
-#red-line {
-  height: 15px;
-  border-radius: 18px;
-  background-color: var(--vivid-red);
-  transition: width 0.2s ease-in-out;
-}
-
-#timer-count {
-  font-size: 8rem;
-}
-
-%timer-button {
-  padding: 1rem;
-  width: 180px;
-  border-radius: 18px;
-  font-size: 120%;
-  font-family: sans-serif;
-  color: white;
-  font-weight: 900;
-  border: none;
-  margin-top: 1rem;
-
+%btn {
   &:hover, &:focus {
     cursor: pointer;
   }
 }
 
-h2 {
-  margin: 2rem;
+@mixin timer-setter($bg-color) {
+  background-color: $bg-color;
+  padding: 0.5rem 0.8rem;
+  border-radius: 8px;
+  font-weight: 600;
+
+  @extend %btn;
 }
 
-
-#start-timer-btn {
-  @extend %timer-button;
-  width: 50%;
-  background-color: var(--vivid-red);
-}
-
-#stop-timer-btn {
-  @extend %timer-button;
-  background-color: var(--light-blue);
-}
-
-#restart-timer-btn {
-  @extend %timer-button;
-  background-color: darkorange;
-  margin-left: 1rem;
-}
-
-@media (max-width: 768px) {
-  #timer-count {
-    font-size: 7rem;
-  }
+@mixin line($color) {
+  height: 15px;
+  border-radius: 18px;
+  background-color: $color;
   
+  transition: background-color 0.2s ease-in-out;
+}
+
+#timer-container {
+
+  #timer-setters {
+    display: flex;
+    gap: 0.6rem;
+    margin-bottom: 1rem;
+  }
+
+  #line {
+    transition: width 0.2s ease-in-out;
+  }
+
+  .pomo {
+    @include line(var(--vivid-red))
+  }
+
+  .short {
+    @include line(var(--short-rest))
+  }
+
+  .long {
+    @include line(var(--long-rest))
+  }
+
+  #timer-count {
+    font-size: 8rem;
+  }
+
+  @mixin timer-button($bg-color) {
+    padding: 1rem;
+    width: 160px;
+    border-radius: 18px;
+    font-size: 1.5rem;
+    background-color: $bg-color;
+    color: white;
+    font-weight: 900;
+    border: none;
+    margin-top: 1rem;
+
+    @extend %btn
+  }
+
+  #normal-pomo {
+    @include timer-setter(var(--vivid-red));
+  }
+
+  #short-rest {
+    @include timer-setter(var(--short-rest));
+  }
+
+  #long-rest {
+    @include timer-setter(var(--long-rest));
+  }
+
+  .done {
+    display: block;
+    margin-top: 1rem;
+    font-weight: 700;
+    font-size: 2rem;
+  }
+
   #start-timer-btn {
-    margin-top: 0;
-    width: 200px;
+    @include timer-button(var(--vivid-red));
   }
 
   #stop-timer-btn {
-    width: 100px;
+    @include timer-button(var(--light-blue));
   }
 
   #restart-timer-btn {
-    width: 100px;
+    @include timer-button(darkorange);
+    margin-left: 1rem;
   }
+}
+
+
+@media (max-width: 768px) {
+  @mixin mobile-btn($width) {
+    margin-top: 0;
+    width: $width;
+  }
+  #timer-container {
+    width: 80vw;
+    #timer-count {  
+      font-size: 6rem;
+    }
+    #start-timer-btn {
+      @include mobile-btn(200px)
+    }
+
+    #stop-timer-btn {
+      @include mobile-btn(120px)
+    }
+
+    #restart-timer-btn {
+      @include mobile-btn(140px)
+    }
+  }
+  
+  
 }
 </style>
--- client/src/stores/timer.ts
+++ client/src/stores/timer.ts
@@ -1,17 +1,40 @@
 import { defineStore } from 'pinia'
 import dayjs from 'dayjs'
 
-const MINUTES25 = dayjs().set('minutes', 25).set('seconds', 0)
-const MINUTESINSECONDS = 1500
+type TimerType = {
+  minutes: dayjs.Dayjs;
+  seconds: number;
+}
+
+const MINUTES25: TimerType = {
+  minutes: dayjs().set('minutes', 25).set('seconds', 0),
+  seconds: 1500
+}
+const LONG_REST: TimerType = {
+  minutes: dayjs().set('minutes', 15).set('seconds', 0),
+  seconds: 900
+}
+const SHORT_REST: TimerType = {
+  minutes: dayjs().set('minutes', 5).set('seconds', 0),
+  seconds: 300
+}
+
+function calculatePercent(time: TimerType, timer: dayjs.Dayjs) {
+  return time.minutes.diff(timer, 'seconds') /
+  time.seconds * 100
+}
 
 export const useTimerStore = defineStore({
   id: 'timer',
   state: () => ({
     timerId: 0,
-    timer: MINUTES25,
+    timer: MINUTES25.minutes,
     done: false,
     ongoing: false,
-    percent: 10
+    current: 'pomo',
+    percent: 5,
+    sessions: 0,
+    minutesInSeconds: 1500
   }),
   getters: {
     minutes: state => state.timer.minute(),
@@ -26,16 +49,44 @@ export const useTimerStore = defineStore({
     },
     markDone() {
       this.done = true
+      // set next timer
+      if (this.current === 'short' || this.current === 'long') {
+        this.setPomo()
+      } else if (this.current === 'pomo') {
+        if (this.sessions === 3) {
+          this.setLongRest()
+          this.sessions = 0
+        } else {
+          this.setShortRest()
+          this.sessions++
+        }
+      }
     },
     incrementLine() {
       // Compare the current time with the original time
-      const diff = MINUTES25.diff(this.timer, 'seconds')
-      this.percent = ((diff / MINUTESINSECONDS) * 80) + 5
-      
+      switch (this.current) {
+        case 'pomo':
+          this.percent = calculatePercent(MINUTES25, this.timer) + 5
+          break;
+        case 'short':
+          this.percent = calculatePercent(SHORT_REST, this.timer) + 5
+          break;
+        case 'long':
+          this.percent = calculatePercent(LONG_REST, this.timer) + 5
+      }
+      console.log(this.percent)
     },
-    setInitialValue() {
-      this.timer = MINUTES25
-      this.done = false
+    setPomo() {
+      this.timer = MINUTES25.minutes
+      this.current = 'pomo'
+    },
+    setShortRest() {
+      this.timer = SHORT_REST.minutes
+      this.current = 'short'
+    },
+    setLongRest() {
+      this.timer = LONG_REST.minutes
+      this.current = 'long'
     }
   }
 })
