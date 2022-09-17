48db6b6def82e52a598cd038f7e8e917467a9692
icarusgk <icarus.gk@gmail.com>
2022-09-17 12:40:48 -0600

    add: Stats model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 class Tag(models.Model):
   name = models.CharField(max_length=20, unique=True)
@@ -43,3 +44,12 @@ class Project(models.Model):
 
   def __str__(self):
     return f'Project: {self.name}'
+
+
+class Stats(models.Model):
+  day = models.DateField(default=timezone.now, null=False)
+  done = models.IntegerField(default=0)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stats')
+
+  def __str__(self):
+    return f'{self.user} has done {self.done} on {self.day}'
