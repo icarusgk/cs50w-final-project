4b07fe4126463e9ffea792449172c398e8768b44
icarusgk <icarus.gk@gmail.com>
2022-09-21 17:06:14 -0600

    add: ModeType

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,6 +2,7 @@ import type Project from './ProjectType';
 import type TaskType from './TaskType';
 import type Tag from './TagType';
 import type SubtaskType from './SubtaskType';
-import type StatType from './StatType'
+import type StatType from './StatType';
+import type ModeType from './ModeType';
 
-export { Project, TaskType, Tag, SubtaskType, StatType };
+export { Project, TaskType, Tag, SubtaskType, StatType, ModeType };
