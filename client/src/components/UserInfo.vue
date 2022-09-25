fa5e92e846cddb234499d78d93c0e00aefb66a2b
icarusgk <icarus.gk@gmail.com>
2022-09-25 10:53:57 -0600

    refactor: move modal content to its own component
    and rename variables

---
+++ client/src/components/UserInfo.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+
+defineEmits(['exitModal'])
+
+const auth = useAuthStore();
+</script>
+
+<template>
+  <div>
+    <h1>Logout</h1>
+    <h2>Hello there {{ auth.user?.username }}</h2>
+    <button
+      id="logout-btn"
+      @click="
+        auth.logout();
+        $emit('exitModal')
+      "
+    >
+      Logout
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#logout-btn {
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  border: none;
+  background-color: var(--vivid-red);
+  color: white;
+  font-weight: 500;
+  transition: background-color 0.15s ease-in;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #ff4b4b9f;
+  }
+}
+</style>
