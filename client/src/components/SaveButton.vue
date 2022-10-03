c446cc8b7e8b1eb66f85179baed222a730bc1116
icarusgk <icarus.gk@gmail.com>
2022-10-03 13:19:14 -0600

    add: depth to buttons

--- client/src/components/SaveButton.vue
+++ client/src/components/SaveButton.vue
@@ -16,12 +16,14 @@ defineProps<{
 
 <style scoped lang="scss">
 .close-modal-button {
-  background-color: var(--vivid-red);
+  $vivid-red: #ed4747;
+  background-color: $vivid-red;
   color: var(--white);
+  box-shadow: 0 3px 0 1px lighten($vivid-red, 10%);
+  transition: box-shadow 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
 
   font: {
-    weight: 900;
-    family: sans-serif;
+    weight: 700;
   }
 
   width: 100%;
@@ -31,8 +33,13 @@ defineProps<{
   padding: 0.8rem 0.8rem;
 
   &:hover,
-  &:focus {
+  &:focus,
+  &:active {
     cursor: pointer;
+  }
+
+  &:active {
+    box-shadow: 0 0 $vivid-red;
   }
 }
 
--- client/src/components/buttons/ChoreButton.vue
+++ client/src/components/buttons/ChoreButton.vue
@@ -19,12 +19,19 @@ import AddIcon from '@/components/icons/AddIcon.vue';
   border-radius: 10px;
   margin-right: 1rem;
   padding: 0.1rem 0.5rem;
+  // Add depth to the button
+  box-shadow: 3px 4px rgba(208, 208, 208);
+  transition: box-shadow 0.1s cubic-bezier(0.075, 0.82, 0.165, 1);
 
   &:hover,
   &:focus {
     cursor: pointer;
   }
 
+  &:active {
+    box-shadow: 0 0 var(--white);
+  }
+  
   .add-icon {
     width: 2rem;
     margin-right: 0.4rem;
