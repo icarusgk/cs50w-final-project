aa54d9c1e07aedb4edb97d110c90205b05b02002
icarusgk <icarus.gk@gmail.com>
2022-10-05 18:06:09 -0600

    feat: clicking on current task opens it

--- client/src/components/CurrentTask.vue
+++ client/src/components/CurrentTask.vue
@@ -2,10 +2,16 @@
 import { ref, watch, computed, onMounted } from 'vue';
 import { useChoreStore } from '@/stores/chore';
 import { useAuthStore } from '@/stores/auth';
+import { useModalStore } from '@/stores/modal';
 import axios from 'axios';
+import TaskModal from './TaskModal.vue';
+import TaskInfoIcon from './icons/TaskInfoIcon.vue';
 
 const task = ref();
-const currentTask = computed(() => useChoreStore().currentTaskId);
+const chore = useChoreStore();
+const currentTask = computed(() => chore.currentTaskId);
+const open = ref(false);
+
 
 async function getTask() {  
   const response = await axios.get(`tasks/${currentTask.value}/`);
@@ -25,13 +31,48 @@ watch(
     if (prevTask !== curTask) getTask();
   }
 );
+watch(open, () => {
+  useModalStore().toggle();
+});
 </script>
 
 <template>
   <div 
-    v-if="useAuthStore().isAuthenticated && useChoreStore().currentTaskId" 
+    v-if="useAuthStore().isAuthenticated && chore.currentTaskId" 
     style="margin-top: 1rem"
   >
-    <span>Working on task: {{ task?.title }}</span>
+    <div @click="open = true" class="container">
+      <div class="info">
+        <TaskInfoIcon />
+        <span>Working on task: {{ task?.title }}</span>
+      </div>
+    </div>
+    <TaskModal :open="open" :task="task" @exit="open = false" />
   </div>
 </template>
+
+
+<style scoped lang="scss">
+  .container {
+    background: #333;
+    color: white;
+    padding: 1rem;
+    border-radius: 10px;
+    font-weight: 600;
+
+    .info {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 0.5rem;
+      }
+    }
+
+    &:hover,
+    &:focus,
+    &:active {
+      cursor: pointer;
+    }
+  }
+</style>
