da313ec1ed98e77be91b230f5d53a9d311655392
icarusgk <icarus.gk@gmail.com>
2022-10-15 13:12:55 -0600

    feat: remove current task

--- client/src/components/CurrentTask.vue
+++ client/src/components/CurrentTask.vue
@@ -2,12 +2,17 @@
 import { ref, watch, computed, watchEffect } from 'vue';
 import { useAuthStore } from '@/stores/auth';
 import { useModalStore } from '@/stores/modal';
+import { useChoreStore } from '@/stores/chore';
 import axios from 'axios';
 import TaskModal from './TaskModal.vue';
 import TaskInfoIcon from './icons/TaskInfoIcon.vue';
+import CloseIcon from './icons/CloseIcon.vue';
+import Popper from 'vue3-popper';
+
 
 const task = ref();
 const auth = useAuthStore();
+const chore = useChoreStore();
 const userTaskId = computed(() => auth.user?.current_task_id);
 const open = ref(false);
 
@@ -36,11 +41,19 @@ watch(open, () => {
       v-if="userTaskId !== 0 && auth.user" 
       style="margin-top: 1rem"
     >
-      <div @click="open = true" class="container">
-        <div class="info">
-          <TaskInfoIcon />
-          <span>Working on task: {{ task?.title }}</span>
+      <div class="current-task-container">        
+        <div @click="open = true" class="container">
+          <div class="info">
+            <TaskInfoIcon />
+            <span>Working on task: {{ task?.title }}</span>
+          </div>
         </div>
+        <Popper hover>
+          <CloseIcon @click="chore.changeCurrentTask(0)" class="close-icon" />
+          <template #content>
+            Remove current task
+          </template>
+        </Popper>
       </div>
       <TaskModal :open="open" :task="task" @exit="open = false" />
     </div>
@@ -59,32 +72,52 @@ watch(open, () => {
   opacity: 0;
 }
 
-.container {
-  width: 21rem;
-  background: #333;
-  color: white;
+:deep(.popper) {
   padding: 1rem;
   border-radius: 10px;
-  font-weight: 600;
-  box-shadow: 3px 4px rgb(71, 71, 71);
-  transition: box-shadow 0.1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  color: #fff;
+  font-weight: bold;
+}
 
-  .info {
-    display: flex;
-    align-items: center;
-
-    span {
-      margin-left: 0.5rem;
+.current-task-container {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  .container {
+    width: 21rem;
+    background: #333;
+    color: white;
+    padding: 1rem;
+    border-radius: 10px;
+    font-weight: 600;
+    box-shadow: 3px 4px rgb(71, 71, 71);
+    transition: box-shadow 0.1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  
+    .info {
+      display: flex;
+      align-items: center;
+  
+      span {
+        margin-left: 0.5rem;
+      }
+    }
+  
+    &:hover,
+    &:focus,
+    &:active {
+      cursor: pointer;
+    }
+    &:active {
+      box-shadow: 0 0 #333;
     }
   }
 
-  &:hover,
-  &:focus,
-  &:active {
-    cursor: pointer;
-  }
-  &:active {
-    box-shadow: 0 0 #333;
+  .close-icon {
+    &:hover,
+    &:focus,
+    &:active {
+      cursor: pointer;
+    }
   }
 }
 </style>
