46fefd456b7830f83ab975e2f9101b6bf36d4eac
icarusgk <icarus.gk@gmail.com>
2022-10-21 15:11:44 -0600

    update: AboutView info

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,11 +1,43 @@
+<script setup>
+import AboutIcon from '@/components/icons/AboutIcon.vue'
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an About page</h1>
+    <div class="title">
+      <AboutIcon />
+      <h1>About</h1>
+    </div>
+
+    <div class="info">
+      <h1>What is Pomodoro?</h1>
+      <p>Pomodoro is a technique for productivity.</p>
+    </div>
+
+    <div class="final-project">
+      <h1>CS50w Final Project</h1>
+      <p>This is my CS50's Web Programming with Python and JavaScript final project.</p>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped lang="scss">
 .about {
   color: white;
+  margin: 2rem 4rem;
+
+  .title {
+    display: flex;
+    align-items: center;
+    margin-left: -0.5rem;
+    margin-bottom: 1rem;
+    h1 {
+      margin-left: 1rem;
+    }
+  }
+
+  .final-project {
+    margin-top: 2rem;
+  }
 }
 </style>
