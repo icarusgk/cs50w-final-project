1ff8902b0ddec2c840e7cb34139ef85dd2d52690
icarusgk <icarus.gk@gmail.com>
2022-10-21 20:39:47 -0600

    refactor: server files with autopep8

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from api import views
 from rest_framework.routers import DefaultRouter
 
 from rest_framework_simplejwt.views import (
-  TokenObtainPairView,
-  TokenRefreshView
+    TokenObtainPairView,
+    TokenRefreshView
 )
 
 router = DefaultRouter()
@@ -16,14 +16,14 @@ router.register(r'stats', views.StatsViewSet)
 router.register(r'modes', views.ModeViewSet)
 
 urlpatterns = [
-  path('', include(router.urls)),
-  path('currentTask/', views.CurrentTaskView.as_view()),
-  path('currentMode/', views.CurrentModeView.as_view()),
-  path('tagInfo/<str:name>/', views.TagInfo.as_view()),
-  
-  # User Router
-  path('register/', views.RegisterView.as_view()),
-  path('me/', views.CurrentUserView.as_view()),
-  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('', include(router.urls)),
+    path('currentTask/', views.CurrentTaskView.as_view()),
+    path('currentMode/', views.CurrentModeView.as_view()),
+    path('tagInfo/<str:name>/', views.TagInfo.as_view()),
+
+    # User Router
+    path('register/', views.RegisterView.as_view()),
+    path('me/', views.CurrentUserView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
