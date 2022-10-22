f89e36b311225b8a1da9d2a1a3757dba05e36f90
icarusgk <icarus.gk@gmail.com>
2022-10-21 20:39:47 -0600

    refactor: server files with autopep8

--- api/serializers.py
+++ api/serializers.py
@@ -1,57 +1,69 @@
 from .models import *
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ['id', 'username', 'email', 'auto_start_pomos', 'auto_start_breaks', 'current_task_id']
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'auto_start_pomos',
+            'auto_start_breaks',
+            'current_task_id']
 
 
 class StatsSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Stats
-    fields = ['id', 'day', 'chores_done']
+    class Meta:
+        model = Stats
+        fields = ['id', 'day', 'chores_done']
 
 
 class ModesSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Mode
-    exclude = ['user']
-    
+    class Meta:
+        model = Mode
+        exclude = ['user']
+
 
 class TagSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Tag
-    fields = ['id', 'name']
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
 
 class TaskSerializer(serializers.ModelSerializer):
-  # user = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Task
+        fields = [
+            'id',
+            'title',
+            'description',
+            'estimated',
+            'gone_through',
+            'minutes',
+            'done',
+            'tags',
+            'subtasks',
+            'project_tasks']
+        depth = 1
 
-
-  class Meta:
-    model = Task
-    fields = ['id', 'title', 'description', 'estimated', 
-             'gone_through', 'minutes', 'done', 'tags', 'subtasks', 'project_tasks']
-    depth = 1
 
 class SubtaskSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Subtask
-    fields = ['id', 'title', 'description', 'done']
+    class Meta:
+        model = Subtask
+        fields = ['id', 'title', 'description', 'done']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-  user = serializers.ReadOnlyField(source='user.username')      
+    user = serializers.ReadOnlyField(source='user.username')
 
+    def get_fields(self):
+        fields = super(ProjectSerializer, self).get_fields()
+        fields['tasks'] = TaskSerializer(many=True)
+        return fields
 
-  def get_fields(self):
-    fields = super(ProjectSerializer, self).get_fields()
-    fields['tasks'] = TaskSerializer(many=True)
-    return fields
-
-
-  class Meta:
-    model = Project
-    fields = ['id', 'name', 'tasks', 'user']
-    depth = 2
-    
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'tasks', 'user']
+        depth = 2
