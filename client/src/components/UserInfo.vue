53ec8427c1d4f58681b7bb76a88643e4d4d031e9
icarusgk <icarus.gk@gmail.com>
2022-10-28 22:00:00 -0600

    Change the user modal for a Popper hover

--- client/src/components/UserInfo.vue
+++ client/src/components/UserInfo.vue
@@ -9,33 +9,9 @@ const auth = useAuthStore();
 <template>
   <div>
     <h2>Hello there {{ auth.user?.username }}!</h2>
-    <button
-      id="logout-btn"
-      @click="
-        auth.logout();
-        $emit('exitModal');
-      "
-    >
-      Logout
-    </button>
   </div>
 </template>
 
 <style scoped lang="scss">
-#logout-btn {
-  padding: 0.5rem 1rem;
-  border-radius: 8px;
-  border: none;
-  background-color: var(--vivid-red);
-  color: white;
-  font-weight: 500;
-  transition: background-color 0.15s ease-in;
 
-  &:hover,
-  &:focus,
-  &:active {
-    cursor: pointer;
-    background-color: #ff4b4b9f;
-  }
-}
 </style>
