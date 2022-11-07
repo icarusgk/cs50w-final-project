4731c793bff40b588e1ebbe2ca9d24cbadf5fbda
icarusgk <icarus.gk@gmail.com>
2022-11-07 11:21:36 -0600

    add: better Tag pages

--- client/src/views/TagView.vue
+++ client/src/views/TagView.vue
@@ -5,16 +5,20 @@ import { useFetch } from '@/composables/useFetch';
 import TaskInfo from '../components/TaskInfo.vue';
 import BackIcon from '@/components/icons/BackIcon.vue';
 import type { TaskType } from '@/types';
+import DeleteIcon from '@/components/icons/DeleteIcon.vue';
 
 const route = useRoute();
 
 const tasks = ref<TaskType[]>([]);
+const fetchedTags = ref(false);
 
-watchEffect(() => {
-  useFetch(`tagInfo/${route.params.name}`, 'get').then((res) => {
-    tasks.value = res.data;
-  });
-})
+watchEffect(async () => {
+  if (route.params.name) {
+    const { data } = await useFetch(`tagInfo/${route.params.name}`, 'get');
+    tasks.value = data as TaskType[];
+    fetchedTags.value = true;
+  }
+});
 </script>
 
 <template>
@@ -22,16 +26,31 @@ watchEffect(() => {
     <div class="go-back">
       <BackIcon class="button" @click="$router.back()" />
     </div>
-    <span class="title">#{{ route.params.name }}</span>
+    <div class="tag-name">
+      <span class="title">#{{ route.params.name }}</span>
+      <div class="delete-tag">
+        <span class="text">Delete tag</span>
+        <div><DeleteIcon /></div>
+      </div>
+    </div>
     <div class="all-tasks-container">
-      <TaskInfo v-for="task in tasks" :task="task" :key="task.id" />
+      <TaskInfo v-if="fetchedTags" 
+        v-for="task in tasks" :task="task" :key="task.id" 
+      />
+      <div v-else>
+        <span class="info">Loading...</span>
+      </div>
+
+      <div v-if="fetchedTags && tasks.length === 0">
+        <span class="info">There are no tasks inside this tag.</span>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .tag-info-container {
-  padding: 2rem 1.5rem;
+  padding: 2rem 4rem;
   .go-back {
     display: flex;
     align-items: center;
@@ -52,10 +71,42 @@ watchEffect(() => {
     }
   }
 
-  .title {
-    color: white;
-    font-size: 2rem;
-    font-weight: 800;
+  .tag-name {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+
+    .title {
+      color: white;
+      font-size: 2rem;
+      font-weight: 800;
+      margin-top: 0.5rem;
+    }
+    .delete-tag {
+      display: flex;
+      align-items: center;
+      background-color: var(--vivid-red);
+      color: white;
+      border-radius: 8px;
+      padding: 0.7rem;
+      box-shadow: 0 3px 0 1px lighten(#ed4747, 10%);
+      transition: box-shadow 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+      &:hover,
+      &:focus,
+      &:active {
+        cursor: pointer;
+      }
+
+      &:active {
+        box-shadow: 0 0 var(--vivid-red);
+      }
+
+      .text {
+        margin-right: 1rem;
+      }
+    }
   }
 
   .all-tasks-container {
@@ -63,6 +114,10 @@ watchEffect(() => {
     flex-direction: row;
     flex-wrap: wrap;
     gap: 1rem;
+    .info {
+      color: white;
+      font-size: 1.1rem;
+    }
   }
 }
 </style>
