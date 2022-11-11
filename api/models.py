c70944bb9347124ef31b27a00b4e6e309ea1bab2
Roger! <icarus.gk@gmail.com>
2022-11-11 17:02:02 -0600

    fix: Tags being unique to the user not the whole app
    and run migrations

--- api/models.py
+++ api/models.py
@@ -8,7 +8,10 @@ class Tag(models.Model):
         'User',
         on_delete=models.CASCADE,
         related_name='tags')
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        unique_together = (('name', 'user'),)
 
     def __str__(self):
         return f'#{self.name}'
