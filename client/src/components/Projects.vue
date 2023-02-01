eeb504c9c57d662e27203afbb7880eb9caf876a9
Roger! <icarus.gk@gmail.com>
2023-02-01 17:13:26 -0600

    remove: unused `ref` import

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { computed } from 'vue';
 import { useChoreStore } from '@/stores/chore';
 
 import Project from '@/components/buttons/Project.vue';
