f2cdd106060985425d61d50b82121d67e20c1a8d
Roger! <icarus.gk@gmail.com>
2023-02-01 19:55:36 -0600

    add: basic auto-import config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,30 @@ import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), AutoImport({
+    include: [
+      /\.vue$/, /\.vue\?vue/, // .vue
+    ],
+    imports: [
+      'vue',
+      {
+        'axios': [
+          // default imports
+          ['default', 'axios'], // import { default as axios } from 'axios',
+        ],
+      }
+    ],
+    dirs: ['./src/stores'],
+    vueTemplate: true,
+    dts: 'auto-imports.d.ts',
+    eslintrc: {
+      enabled: true
+    }
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
