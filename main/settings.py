9c858278c9e057f1efc654245cff0edb225b4e27
Roger! <icarus.gk@gmail.com>
2023-02-07 14:35:41 -0600

    fix: JWT timedelta settings

--- main/settings.py
+++ main/settings.py
@@ -102,8 +102,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7),
 
     # For custom auth
     'AUTH_COOKIE': 'access_token',      # Cookie name. Enables cookies if value is set
