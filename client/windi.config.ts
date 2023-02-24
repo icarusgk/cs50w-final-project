1fdf754f953d430580e71c954ec6ae3bdd3d81db
Roger! <icarus.gk@gmail.com>
2023-02-24 15:38:17 -0600

    ✨  add: theme config

--- client/windi.config.ts
+++ client/windi.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
-  darkMode: 'class'
+  darkMode: 'class',
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1160px',
+      },
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif'],
+      },
+    }
+  }
 })
