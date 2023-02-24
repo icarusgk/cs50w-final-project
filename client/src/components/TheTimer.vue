18c04216b919325e6fdbc232b10eca68df7e5ffa
Roger! <icarus.gk@gmail.com>
2023-02-24 17:24:30 -0600

    🎨 refactor: TheTimer

--- client/src/components/TheTimer.vue
+++ client/src/components/TheTimer.vue
@@ -78,191 +78,39 @@ function setTimer(type: string) {
 </script>
 
 <template>
-  <div id="timer-container">
-    <span v-if="!timer.currentMode.includes('Default')" id="mode-title">
+  <div class="w-3/4 lg:w-[550px]">
+    <span v-if="!timer.currentMode.includes('Default')" class="block text-2xl font-bold">
       Current Mode: {{ timer.currentMode }}
     </span>
     <!-- Timer buttons -->
-    <div id="timer-setters">
-      <div id="normal-pomo" @click="setTimer('pomo')">Pomo</div>
-      <div id="short-rest" @click="setTimer('short_break')">Short Rest</div>
-      <div id="long-rest" @click="setTimer('long_break')">Long Rest</div>
+    <div class="flex gap-2 mb-8">
+      <div class="timer-btn timer-btn-red" @click="setTimer('pomo')">Pomo</div>
+      <div class="timer-btn timer-btn-blue" @click="setTimer('short_break')">Short Rest</div>
+      <div class="timer-btn timer-btn-green" @click="setTimer('long_break')">Long Rest</div>
     </div>
     <!-- Time -->
     <div>
-      <h1 id="timer-count">{{ timer.formattedTime }}</h1>
+      <h1 class="font-bold my-4 text-8xl lg:text-9xl">{{ timer.formattedTime }}</h1>
       <button
-        @click="
-          startTimer();
-          startAudio.play();
-        "
+        @click="startTimer(); startAudio.play();"
         v-if="!timer.ongoing"
         :class="timer.current"
       >
         Start!
       </button>
-      <button @click="stopTimer()" v-else id="stop-timer-btn">Stop!</button>
+      <button @click="stopTimer()" v-else class="stop-timer-btn">Stop!</button>
       <button
         @click="restartTimer()"
         v-if="timer.ongoing"
-        id="restart-timer-btn"
+        class="restart-timer-btn ml-4"
       >
         Restart!
       </button>
       <!-- TODO: Change style -->
-      <div class="done" v-if="timer.done && !timer.ongoing">
+      <div class="block mt-4 font-bold text-4xl" v-if="timer.done && !timer.ongoing">
         <span>Done!</span>
       </div>
       <CurrentTask />
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-@mixin btn($bg-color) {
-  box-shadow: 0 4px 0 0 lighten($bg-color, 10%);
-  transition: box-shadow 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
-  &:hover,
-  &:focus,
-  &:active {
-    cursor: pointer;
-  }
-
-  &:active {
-    box-shadow: 0 0 $bg-color;
-  }
-}
-
-@mixin timer-setter($bg-color) {
-  @include btn($bg-color);
-
-  background-color: $bg-color;
-  padding: 0.5rem 0.8rem;
-  border-radius: 8px;
-  font-weight: 600;
-}
-
-@mixin line($color) {
-  height: 15px;
-  border-radius: 18px;
-  background-color: $color;
-
-  transition: background-color 0.2s ease-in-out;
-}
-
-#timer-container {
-  #mode-title {
-    display: block;
-    font-size: 1.5rem;
-    font-weight: 700;
-    margin-bottom: 1rem;
-  }
-  #timer-setters {
-    display: flex;
-    gap: 0.6rem;
-    margin-bottom: 1rem;
-  }
-
-  #line {
-    transition: width 0.2s ease-in-out;
-  }
-
-  .line-pomo {
-    @include line(var(--vivid-red));
-  }
-
-  .line-short_break {
-    @include line(var(--short-rest));
-  }
-
-  .line-long_break {
-    @include line(var(--long-rest));
-  }
-
-  #timer-count {
-    font-size: 8rem;
-  }
-
-  @mixin timer-button($bg-color) {
-    @include btn($bg-color);
-
-    padding: 1rem;
-    width: 160px;
-    border-radius: 18px;
-    font-size: 1.5rem;
-    background-color: $bg-color;
-    color: white;
-    font-weight: 900;
-    border: none;
-    margin-top: 1rem;
-  }
-
-  #normal-pomo {
-    @include timer-setter(#ed4747);
-  }
-
-  #short-rest {
-    @include timer-setter(#1ba7b1);
-  }
-
-  #long-rest {
-    @include timer-setter(#1eaf58);
-  }
-
-  .pomo {
-    @include timer-button(#ed4747);
-  }
-
-  .short_break {
-    @include timer-button(#1ba7b1);
-  }
-
-  .long_break {
-    @include timer-button(#1eaf58);
-  }
-
-  .done {
-    display: block;
-    margin-top: 1rem;
-    font-weight: 700;
-    font-size: 2rem;
-  }
-
-  #start-timer-btn {
-    @include timer-button(#ed4747);
-  }
-
-  #stop-timer-btn {
-    @include timer-button(#1b6eb1);
-  }
-
-  #restart-timer-btn {
-    @include timer-button(darkorange);
-    margin-left: 1rem;
-  }
-}
-
-@media (max-width: 768px) {
-  @mixin mobile-btn($width) {
-    margin-top: 0;
-    width: $width;
-  }
-  #timer-container {
-    width: 82vw;
-    #timer-count {
-      font-size: 6rem;
-    }
-    #start-timer-btn {
-      @include mobile-btn(200px);
-    }
-
-    #stop-timer-btn {
-      @include mobile-btn(120px);
-    }
-
-    #restart-timer-btn {
-      @include mobile-btn(140px);
-    }
-  }
-}
-</style>
