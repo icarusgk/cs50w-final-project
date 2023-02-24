94a5e564fa1c00a6c8f571617bac325f7fc0433e
Roger! <icarus.gk@gmail.com>
2023-02-24 13:53:36 -0600

    ✨  add: windiCSS lib and config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    WindiCSS(),
     AutoImport({
       include: [
         /\.vue$/,
