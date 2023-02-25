828765b0d0b5927af0fc24392efc39475a75c570
Roger! <icarus.gk@gmail.com>
2023-02-24 23:08:40 -0600

    🎨  refactor: Base and Projects - Tasks View

--- client/src/components/BaseViewProject.vue
+++ client/src/components/BaseViewProject.vue
@@ -13,28 +13,8 @@ watch(open, () => {
 </script>
 
 <template>
-  <div class="single-project" @click="open = true">
+  <div class="w-full p-4 bg-[#3d3d3d] rounded-lg pointer md:w-auto" @click="open = true">
     <h1>{{ project.name }}</h1>
   </div>
   <TheProjectModal :project="project" :open="open" @exit:modal="open = false" />
 </template>
-
-<style scoped lang="scss">
-.single-project {
-  padding: 1rem;
-  background-color: #3d3d3d;
-  border-radius: 8px;
-
-  &:hover,
-  &:focus,
-  &:active {
-    cursor: pointer;
-  }
-}
-
-@media (max-width: 480px) {
-  .single-project {
-    width: 100%;
-  }
-}
-</style>
