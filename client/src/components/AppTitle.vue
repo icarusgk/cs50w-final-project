de5dd3ef1cde5759c321d663e2aba56b42c3cc4a
Roger! <icarus.gk@gmail.com>
2023-02-24 22:14:16 -0600

    🎨  refactor: AppTitle.vue

--- client/src/components/AppTitle.vue
+++ client/src/components/AppTitle.vue
@@ -1,42 +1,9 @@
 <template>
   <!-- Pomo Title -->
-  <div @click="$router.push('/')" id="pomodoro-title-container">
+  <div @click="$router.push('/')" class="pointer flex items-center m-0">
     <!-- Icon -->
-    <LightingIcon id="pomo-icon" />
+    <LightingIcon class="transform scale-65 xl:scale-75" />
     <!-- Title -->
-    <h1 id="title">Pomodoro Timer</h1>
+    <h1 class="text-sm -ml-3 md:text-xl md:-ml-0 xl:text-3xl font-bold">Pomodoro Timer</h1>
   </div>
 </template>
-
-<style scoped lang="scss">
-#pomodoro-title-container {
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  #title {
-    font-size: 2rem;
-  }
-}
-
-@media (max-width: 768px) {
-  #pomodoro-title-container {
-    margin: 0;
-
-    #pomo-icon {
-      transform: scale(0.7);
-    }
-    #title {
-      margin-left: -8px;
-      font-size: 1.2rem;
-    }
-  }
-}
-
-@media (max-width: 460px) {
-  #pomodoro-title-container {
-    #title {
-      font-size: 0.8rem;
-    }
-  }
-}
-</style>
