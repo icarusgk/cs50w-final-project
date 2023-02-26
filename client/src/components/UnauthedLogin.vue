88510cd27cb6813afa5167c3d21a7e6ce0a8f0a5
Roger! <icarus.gk@gmail.com>
2023-02-26 16:51:02 -0600

    🎨  migrate: UnauthedLogin.vue

--- client/src/components/UnauthedLogin.vue
+++ client/src/components/UnauthedLogin.vue
@@ -1,51 +1,11 @@
 <template>
-  <div id="login-signup">
-    <router-link to="/login">Login</router-link>
+  <div class="text-2xl mt-8 md:text-3xl block text-[2rem] font-semibold p-4 bg-vivid-red rounded-lg">
+    <router-link class="no-underline text-white transition-all duration-200 ease-in hover:decoration-solid hover:text-[rgb(255,201,201)]" to="/login">Login</router-link>
     <span> or </span>
-    <router-link to="/register">Sign up</router-link>
+    <router-link class="no-underline text-white transition-all duration-200 ease-in hover:decoration-solid hover:text-[rgb(255,201,201)]" to="/register">Sign up</router-link>
   </div>
-  <div id="message">
+  <div class="py-4 px-0 font-medium text-xl">
     <slot></slot>
   </div>
 </template>
 
-<style scoped lang="scss">
-a {
-  text-decoration: none;
-  color: white;
-  transition: color 0.2s ease-in;
-}
-a:hover {
-  text-decoration: solid;
-  color: rgb(255, 201, 201);
-}
-#login-signup {
-  display: block;
-  font-size: 2rem;
-  font-weight: 600;
-  padding: 1rem;
-  background-color: var(--vivid-red);
-  border-radius: 10px;
-}
-
-#message {
-  display: block;
-  padding: 1rem 0;
-  font-weight: 500;
-  font-size: 1.2rem;
-}
-
-@media (max-width: 768px) {
-  #login-signup {
-    font-size: 1.8rem;
-    margin-top: 2rem;
-  }
-}
-
-@media (max-width: 500px) {
-  #login-signup {
-    font-size: 1.5rem;
-    margin-top: 2rem;
-  }
-}
-</style>
