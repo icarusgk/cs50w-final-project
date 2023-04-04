ffad9dc12668103ff139a7332d37136e918e5556
Roger! <icarus.gk@gmail.com>
2023-04-04 07:19:15 -0600

    ⚰️  remove: Subtask type

--- client/src/types/ISubtask.ts
+++
@@ -1,8 +0,0 @@
-interface Subtask {
-  id?: number;
-  title: string;
-  description: string;
-  done?: boolean;
-}
-
-export default Subtask;
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,9 +1,8 @@
 import type IProject from './IProject';
 import type ITask from './ITask';
 import type ITag from './ITag';
-import type ISubtask from './ISubtask';
 import type IStat from './IStat';
 import type IMode from './IMode';
 import type IUser from './IUser'
 
-export { IProject, ITask, ITag, ISubtask, IStat, IMode, IUser };
+export { IProject, ITask, ITag, IStat, IMode, IUser };
