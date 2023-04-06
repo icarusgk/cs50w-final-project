4994261d15a54cb32f83afe8bbc4b3675db65676
Roger! <icarus.gk@gmail.com>
2023-04-06 17:36:26 -0600

    fix: reduce api calls

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,36 +8,41 @@ modal.close();
 
 auth.getUser();
 
-if (auth.isAuthed) {
-  chore.fetchProjects();
-  chore.fetchTasks();
+if (auth.isAuthed) {  
+  watch(
+    [
+      () => chore.projectPagination.page,
+      () => chore.taskPagination.page,
+      () => chore.projectPagination.page_size,
+      () => chore.taskPagination.page_size,
+    ],
+    ([newProjectPage, newTaskPage], [oldProjectPage, oldTaskPage]) => {
+      if (
+        chore.projectPagination.count > 1 &&
+        newProjectPage !== oldProjectPage
+      ) {
+        chore.fetchProjects();
+      }
+      if (chore.taskPagination.count > 1 && newTaskPage !== oldTaskPage) {
+        chore.fetchTasks();
+      }
+    }
+  );
 }
-
-watch(
-  [
-    () => chore.projectPagination.page,
-    () => chore.taskPagination.page,
-    () => chore.projectPagination.page_size,
-    () => chore.taskPagination.page_size,
-  ],
-  ([newProjectPage, newTaskPage], [oldProjectPage, oldTaskPage]) => {
-    if (
-      chore.projectPagination.count > 1 &&
-      newProjectPage !== oldProjectPage
-    ) {
-      chore.fetchProjects();
-    }
-    if (chore.taskPagination.count > 1 && newTaskPage !== oldTaskPage) {
-      chore.fetchTasks();
-    }
-  }
-);
 </script>
 
 <template>
   <TheSidebar />
   <div class="flex ml-0 mb-[140px] lg:ml-[130px] lg:mb-0 relative">
-    <div :class="[{ '<sm:p-4 filter blur-md pointer-events-none duration-150 ease-out': modal.isOpened }, 'w-full flex flex-col']">
+    <div
+      :class="[
+        {
+          '<sm:p-4 filter blur-md pointer-events-none duration-150 ease-out':
+            modal.isOpened,
+        },
+        'w-full flex flex-col',
+      ]"
+    >
       <TheUpperMenu class="pt-6 py-12 pb-0" />
       <RouterView @vnodeUpdated="modal.close()"></RouterView>
     </div>
