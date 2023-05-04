0f7a3e8807a438e55bb9af74a17a93a1d7887245
Roger! <icarus.gk@gmail.com>
2023-05-03 18:34:13 -0600

    add: settings

--- client/src/stores/timer.ts
+++ client/src/stores/timer.ts
@@ -1,27 +1,26 @@
 import { defineStore } from 'pinia';
-import { useAuthStore, useChoreStore } from '@/stores';
+import { useAuthStore, useAlertStore } from '@/stores';
 import { ref, computed } from 'vue';
 import { toTimer, local } from '@/utils';
 import type { ITimer } from '@/types';
 
-type TimerType = 'pomo' | 'short' | 'long';
+type TimerType = 'pomo' | 'short_break' | 'long_break';
 
 // In case the timer object is empty
 export const defaultTimer: ITimer = {
   id: 0,
   name: 'Default',
   pomo: 25,
-  short: 5,
-  long: 15,
+  short_break: 5,
+  long_break: 15,
 };
 
 // If there isn't a timer on the localstorage, set the defaultTimer
 if (!local.get('timer')) local.set('timer', defaultTimer)
-const localTimer = local.get('timer') as ITimer;
 
 export const useTimerStore = defineStore('timer', () => {
   const auth = useAuthStore();
-  const chore = useChoreStore();
+  const alert = useAlertStore();
 
   let timerId: number | null;
 
@@ -29,11 +28,13 @@ export const useTimerStore = defineStore('timer', () => {
   const sessions = ref(0)
   const timerType = ref<TimerType>('pomo')
   const done = ref(false)
-  
+
   const auto_start_pomo = ref(auth.user?.auto_start_pomos || false)
   const auto_start_breaks = ref(auth.user?.auto_start_breaks || false)
 
-  const currentMode = ref<ITimer>({...defaultTimer});
+  const modes = ref<ITimer[]>(local.get('modes'))
+
+  const currentMode = ref(local.get('timer') || modes.value[0]);
 
   const currentTimer = ref(currentMode.value[timerType.value as keyof ITimer])
   const activeTimer = ref(toTimer(currentTimer.value as number))
@@ -48,20 +49,21 @@ export const useTimerStore = defineStore('timer', () => {
     done.value = false;
     timerId = window.setInterval(() => {
       activeTimer.value = activeTimer.value.subtract(1, 'second');
+      document.title = `Pomo.do - ${displayTimer.value}`
     }, 1000);
   }
 
   // Sets the next timer type based on the current timer type 
   // and number of completed sessions.
   function setNextTimer() {
-    if (timerType.value === 'short' || timerType.value === 'long') {
+    if (timerType.value === 'short_break' || timerType.value === 'long_break') {
       setTo('pomo');
     } else if (timerType.value === 'pomo') {
       useChoreStore().incrementGoneThrough();
       if (sessions.value === 3) {
-        setTo('long'); sessions.value = 0;
+        setTo('long_break'); sessions.value = 0;
       } else {
-        setTo('short'); sessions.value++;
+        setTo('short_break'); sessions.value++;
       }
     }
   }
@@ -77,22 +79,31 @@ export const useTimerStore = defineStore('timer', () => {
     activeTimer.value = toTimer(currentTimer.value as number);
   }
 
+  function changeTimer(timerMode: TimerType = 'pomo') {
+    local.set('timer', currentMode.value);
+    currentTimer.value = currentMode.value[timerMode];
+    activeTimer.value = toTimer(currentTimer.value as number);
+  }
+
   function setTo(timerMode: TimerType) {
     stopTimer();
     timerType.value = timerMode;
-    currentTimer.value = currentMode.value[timerMode as keyof ITimer]
-    activeTimer.value = toTimer(currentTimer.value as number);
+    changeTimer(timerMode);
   }
 
   // From here on, will work with settings
   function setNewTimer(data: ITimer) {
-    currentMode.value = data;
+    currentMode.value = { ...data };
+    timerType.value = 'pomo';
+    alert.success(`Mode changed to ${data.name}`)
+    changeTimer();
   }
 
   function setToDefault() {
-    currentMode.value = {...localTimer};
-    timerType.value = 'pomo';
+    currentMode.value = { ...defaultTimer };
+    alert.info('Back to default')
+    changeTimer();
   }
 
-  return { isRunning, done, sessions, timerType, auto_start_pomo, auto_start_breaks, currentMode, currentTimer, activeTimer, displayTimer, startTimer, restartTimer, stopTimer, setTo, setNextTimer, setNewTimer, setToDefault }
+  return { isRunning, done, sessions, timerType, auto_start_pomo, auto_start_breaks, modes, currentMode, currentTimer, activeTimer, displayTimer, startTimer, restartTimer, stopTimer, setTo, setNextTimer, setNewTimer, setToDefault }
 });
