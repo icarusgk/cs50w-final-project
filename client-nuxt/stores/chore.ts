f3c42348eee9f96f03acbfadc1e246027c73f8d2
icarusgk <icarus.gk@gmail.com>
2023-05-15 14:05:52 -0600

    add: stores

---
+++ client-nuxt/stores/chore.ts
@@ -0,0 +1,188 @@
+import type { IProject, ITag, ITask, IStat } from '@/types';
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { useAlertStore, useAuthStore, usePageStore } from '@/stores';
+import { ref } from 'vue';
+import { useLocal } from '@/utils'
+
+export const useChoreStore = defineStore('chores', () => {
+  const alert = useAlertStore();
+  const auth = useAuthStore();
+  const page = usePageStore();
+
+  const tasks = ref<ITask[]>([]);
+  const projects = ref<IProject[]>([]);
+  const tags = ref<ITag[]>([]);
+  const stats = ref<IStat[]>([]);
+  
+  async function fetchModes() {
+    const { status, data } = await axios.get('modes')
+    if (status === 200) useLocal.set('modes', data);
+  }
+  async function fetchStats() {
+    const { data, status } = await axios.get('stats')
+    if (status === 200) stats.value = data;
+  }
+  async function increaseTodayStats() {
+    const date = new Date();
+    date.setHours(date.getHours() - date.getTimezoneOffset() / 60);
+
+    const { data, status } = await axios.post('stats', {
+      day: date.toISOString().slice(0, 10),
+    });
+    
+    if (status === 201) {
+      let stat = stats.value.find((stat) => stat.id === data.id);
+
+      if (stat) stat.chores_done = data.chores_done;
+    }
+  }
+  async function fetchTasks() {
+    const { data, status } = await axios.get('tasks', {
+      params: { 
+        page: page.taskPagination.page,
+        page_size: page.taskPagination.page_size
+      }
+    });
+
+    if (status === 200) {
+      tasks.value = data.results;
+      page.taskPagination.count = data.count;
+    }
+  }
+  async function fetchProjects() {
+    const { data, status } = await axios.get('projects', {
+      params: {
+        page: page.projectPagination.page,
+        page_size: page.projectPagination.page_size
+      }
+    });
+
+    if (status === 200) {
+      projects.value = data.results;
+      page.projectPagination.count = data.count;
+    }
+  }
+  async function fetchTags() {
+    const { data, status } = await axios.get('tags');
+    if (status === 200) {
+      return (tags.value = data);
+    }
+  }
+  async function addTask(task: ITask) {
+    const { status } = await axios.post('tasks', task);
+    if (status === 201) {
+      alert.success(`Task ${task.title} created!`);
+      // Send user to the first page
+      page.taskPagination.page = 1;
+      fetchTasks();
+    }
+  }
+  async function saveTheTask(task: ITask) {
+    const { status } = await axios.put(`tasks/${task.id}`, task);
+    if (status === 200) {
+      alert.success(`'${task.title}' saved!`);
+      fetchTasks();
+    }
+  }
+  async function deleteTheTask(task: ITask) {
+    const { status } = await axios.delete(`tasks/${task.id}`)
+
+    if (status === 204) {
+      // if it is the last
+      if (tasks.value.length === 1 && page.taskPagination.count === 0) {
+        tasks.value = tasks.value.filter((t: ITask) => t.id !== task.id);
+        page.taskPagination.page = 1;
+      } else {
+        fetchTasks();
+      }
+
+      alert.info(`Task '${task.title}' deleted`);
+
+      if (auth.user && task.id) {
+        auth.user.current_task_id = 0;
+      }
+    }
+  }
+  async function addProject(project: IProject) {
+    const { status } = await axios.post('projects', project);
+
+    if (status === 201) {
+      alert.success(`Project ${project.name} created!`);
+      // Send user to the first page
+      page.projectPagination.page = 1;
+      fetchProjects();
+    }
+  }
+  async function saveProject(project: IProject, newProjectName: string) {
+    const { status } = await axios.patch( `/projects/${project.id}/modify_title`,
+      { name: newProjectName }
+    );
+
+    if (status === 200) alert.success('Project saved!');
+  }
+  async function deleteProject(id: number) {
+    const { status } = await axios.delete(`projects/${id}`)
+
+    if (status === 204) {
+      if (projects.value.length === 1 && page.projectPagination.count === 0) {
+        projects.value = projects.value.filter((p: IProject) => p.id !== id);
+        page.projectPagination.page = 1;
+      } else {
+        fetchProjects();
+      }
+      alert.info('Project deleted!');
+    }
+  }
+  async function incrementGoneThrough() {
+    increaseTodayStats();
+    
+    if (auth.user) {
+      const { status } = await axios.patch(`tasks/${auth.user.current_task_id}`, {
+        obj: 'task',
+        action: 'increment_gone_through',
+      });
+
+      if (status === 200) fetchTasks();
+    }
+  }
+  function deleteTask(task: ITask) {
+    // TODO: Replace with Dialog
+    if (window.confirm('Are you sure you want to delete this task?')) {
+      deleteTheTask(task);
+  
+      if (
+        page.taskPagination.page === page.totalTaskPages &&
+        tasks.value.length === 1
+      ) {
+        page.decreaseTaskPagination();
+      }
+    }
+  }
+  async function changeCurrentTask(id: number | undefined) {
+    const { data, status } = await axios.put('currentTask', { id })
+    if (status === 200 && auth.user) {
+      auth.user.current_task_id = data.id;
+    }
+  }
+  async function toggleDone(task: ITask) {
+    const response = await axios.patch(`tasks/${task.id}/`, {
+      obj: 'task',
+      action: 'done',
+    });
+    if (response?.status === 200) {
+      task.done = response.data?.done;
+      fetchProjects();
+    }
+  }
+  function saveTask(oldTask: ITask, newTask: ITask) {
+    saveTheTask(newTask);
+    oldTask = newTask;
+  }
+  
+  return {
+    tasks, projects, tags, stats, fetchModes, fetchStats, increaseTodayStats, fetchTasks, fetchProjects, fetchTags,
+    addTask, saveTask, deleteTask, addProject, saveProject, deleteProject, incrementGoneThrough,
+    changeCurrentTask, toggleDone
+  }
+}, { persist: true });
---
+++ client-nuxt/stores/index.ts
@@ -0,0 +1,6 @@
+export * from './chore';
+export * from './timer';
+export * from './modal';
+export * from './auth';
+export * from './alerts';
+export * from './pagination';
